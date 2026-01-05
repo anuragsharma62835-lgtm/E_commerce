d2d4eb888da31e4a3d2a6e52c6d8d812bfd43c41
Anurag Sharma <anuragsharma62835@gmail.com>
2026-01-05 14:30:59 +0530

    commit

--- backend/server.js
+++ backend/server.js
@@ -1,43 +1,51 @@
 const express = require("express");
 const app = express();
-const path = require('path')
-const cors = require('cors');
+const path = require("path");
+const cors = require("cors");
 require("dotenv").config();
-const passport = require('passport')
-require('./Config/passport')
-const upload = require("./middleware/upload")
+const passport = require("passport");
+require("./Config/passport");
+const upload = require("./middleware/upload");
 const database = require("./Database/Database");
-const userRoutes = require('./Routes/UserRoutes')
-const productRoutes = require('./Routes/ProductRoutes')
+const userRoutes = require("./Routes/UserRoutes");
+const productRoutes = require("./Routes/ProductRoutes");
 const apilimiter = require("./middleware/ratelimiter");
 const errorhandler = require("./middleware/errorhandler");
-const authRoutes = require("./Routes/AuthRoutes")
-const orderRoutes = require('./Routes/OrderRoutes')
-const adminRoutes = require("./Routes/adminroutes")
+const authRoutes = require("./Routes/AuthRoutes");
+const orderRoutes = require("./Routes/OrderRoutes");
+const adminRoutes = require("./Routes/adminroutes");
 
 database;
 
-app.use(cors({
-  origin:['e-commerce-5jru.vercel.app']
-}))
+app.use(
+  cors({
+    origin: [
+      "e-commerce-5jru.vercel.app",
+      "http://localhost:5173",
+      "e-commerce-git-main-anurags-projects-6158872e.vercel.app",
+    ],
+  })
+);
 
 app.use(express.urlencoded({ extended: true }));
 
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
 app.use(express.json());
-app.use('/api',apilimiter)
+app.use("/api", apilimiter);
 app.use(errorhandler);
 app.use("/uploads", express.static("uploads"));
-app.use('/api/product',productRoutes)
-app.use('/api/user',userRoutes)
-app.use('/api/auth',authRoutes)
+app.use("/api/product", productRoutes);
+app.use("/api/user", userRoutes);
+app.use("/api/auth", authRoutes);
 app.use("/api/order", orderRoutes);
-app.use("/api/admin",adminRoutes)
-
+app.use("/api/admin", adminRoutes);
 
 app.get("/", async (req, res) => {
   res.send("working");
 });
 
+app.listen(5000, () => {
+  console.log("server running");
+});
 module.exports = app
